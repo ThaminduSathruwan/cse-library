7aea4d4430a0772d64231423095915e88bd67496
Jathavan Mahendrarajah <73867299+Rickfdalton@users.noreply.github.com>
2021-09-23 11:43:22 +0530

    Add files via upload

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 
 
 public class MathOperation {
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
 
     public static double add(double... operands)
     {
